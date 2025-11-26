8ceb956ce189e20bdc4059d5ac58d1614e652c8c
JC Heyer <4446535+jcheyer@users.noreply.github.com>
2025-11-26 11:50:32 +0100

    demo

---
+++ go.mod
@@ -0,0 +1,14 @@
+module github.com/jcheyer/gocrondebug
+
+go 1.25.3
+
+require (
+	github.com/davecgh/go-spew v1.1.1
+	github.com/go-co-op/gocron/v2 v2.17.0
+)
+
+require (
+	github.com/google/uuid v1.6.0 // indirect
+	github.com/jonboulle/clockwork v0.5.0 // indirect
+	github.com/robfig/cron/v3 v3.0.1 // indirect
+)
---
+++ main.go
@@ -0,0 +1,49 @@
+package main
+
+import (
+	"fmt"
+	"log"
+
+	"github.com/davecgh/go-spew/spew"
+	"github.com/go-co-op/gocron/v2"
+)
+
+func main() {
+
+	//create the gocron scheduler
+	scheduler, err := gocron.NewScheduler(
+	//gocron.WithLocation(time.UTC),
+	)
+	if err != nil {
+		log.Panicf("failed to create gocron scheduler: %v", err)
+	}
+
+	// Start the scheduler and print the next run time of the job
+	scheduler.Start()
+
+	//create the job with start date in the past
+
+	job, err := scheduler.NewJob(
+		gocron.CronJob("TZ=Europe/Berlin 8 10 * * 1-5", true),
+		gocron.NewTask(func() {
+			fmt.Println("Job executed")
+		}),
+		//gocron.WithSingletonMode(gocron.LimitModeReschedule),
+	)
+	if err != nil {
+		log.Panicf("failed to create job: %v", err)
+	}
+	// !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+	// Double start the scheduler produces
+	// weird behaviour
+	// !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+
+	scheduler.Start()
+
+	nextRuns, err := job.NextRuns(5)
+	if nextRuns[0].Equal(nextRuns[1]) {
+		spew.Dump(nextRuns)
+	}
+
+	scheduler.Shutdown()
+}
