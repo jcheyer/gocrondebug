97f62dd6de7c92aa033c63267a6cf277dea0d509
JC Heyer <4446535+jcheyer@users.noreply.github.com>
2025-11-26 15:41:17 +0100

    add test cases

--- go.mod
+++ go.mod
@@ -4,11 +4,15 @@ go 1.25.3
 
 require (
 	github.com/davecgh/go-spew v1.1.1
-	github.com/go-co-op/gocron/v2 v2.17.0
+	github.com/go-co-op/gocron/v2 v2.18.1
+	github.com/jonboulle/clockwork v0.5.0
+	github.com/stretchr/testify v1.11.1
 )
 
 require (
 	github.com/google/uuid v1.6.0 // indirect
-	github.com/jonboulle/clockwork v0.5.0 // indirect
+	github.com/kr/pretty v0.3.1 // indirect
+	github.com/pmezard/go-difflib v1.0.0 // indirect
 	github.com/robfig/cron/v3 v3.0.1 // indirect
+	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
---
+++ main_test.go
@@ -0,0 +1,140 @@
+package main_test
+
+import (
+	"fmt"
+	"testing"
+	"time"
+
+	"github.com/davecgh/go-spew/spew"
+	"github.com/go-co-op/gocron/v2"
+	"github.com/jonboulle/clockwork"
+	"github.com/stretchr/testify/require"
+)
+
+func TestDoubleStartScheduler(t *testing.T) {
+	r := require.New(t)
+
+	cet, err := time.LoadLocation("CET")
+	r.NoError(err)
+
+	now := time.Now()
+
+	fakeClock := clockwork.NewFakeClockAt(now)
+	later := now.Add(23*time.Hour + 58*time.Minute)
+
+	scheduler, err := gocron.NewScheduler(
+		gocron.WithClock(fakeClock),
+		gocron.WithLocation(cet),
+	)
+	r.NoError(err)
+
+	// Start the scheduler and print the next run time of the job
+	scheduler.Start()
+
+	//create the job with start date in the past
+
+	crontab := fmt.Sprintf("TZ=CET %d %d * * *", later.Minute(), later.Hour())
+
+	executions := 0
+	_, err = scheduler.NewJob(
+		gocron.CronJob(crontab, true),
+		gocron.NewTask(func() {
+			executions++
+		}),
+		gocron.WithSingletonMode(gocron.LimitModeReschedule),
+	)
+	r.NoError(err)
+
+	// !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+	// Double start the scheduler produces
+	// weird behaviour
+	// !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+
+	scheduler.Start()
+
+	r.Equal(0, executions)
+
+	fakeClock.Advance(10 * time.Minute)
+
+	r.Eventually(func() bool {
+		spew.Dump(executions)
+		return executions == 0
+	}, 1*time.Second, 100*time.Millisecond)
+
+	fakeClock.Advance(24 * time.Hour)
+
+	// Executed Twice due to double start ?
+
+	r.Eventually(func() bool {
+		spew.Dump(executions)
+		return executions == 2
+	}, 1*time.Second, 100*time.Millisecond)
+
+	fakeClock.Advance(24 * time.Hour)
+
+	// Never executed !!!!°!
+
+	r.Eventually(func() bool {
+		spew.Dump(executions)
+		return executions == 3
+	}, 1*time.Second, 100*time.Millisecond)
+
+	scheduler.Shutdown()
+}
+
+func TestJustFineWithoutDoubleStartScheduler(t *testing.T) {
+	r := require.New(t)
+
+	cet, err := time.LoadLocation("CET")
+	r.NoError(err)
+
+	now := time.Now()
+
+	fakeClock := clockwork.NewFakeClockAt(now)
+	later := now.Add(23*time.Hour + 58*time.Minute)
+
+	scheduler, err := gocron.NewScheduler(
+		gocron.WithClock(fakeClock),
+		gocron.WithLocation(cet),
+	)
+	r.NoError(err)
+
+	// Start the scheduler and print the next run time of the job
+	scheduler.Start()
+
+	//create the job with start date in the past
+
+	crontab := fmt.Sprintf("TZ=CET %d %d * * *", later.Minute(), later.Hour())
+
+	executions := 0
+	_, err = scheduler.NewJob(
+		gocron.CronJob(crontab, true),
+		gocron.NewTask(func() {
+			executions++
+		}),
+		gocron.WithSingletonMode(gocron.LimitModeReschedule),
+	)
+	r.NoError(err)
+
+	r.Equal(0, executions)
+
+	fakeClock.Advance(10 * time.Minute)
+
+	r.Eventually(func() bool {
+		return executions == 0
+	}, 1*time.Second, 100*time.Millisecond)
+
+	fakeClock.Advance(24 * time.Hour)
+
+	r.Eventually(func() bool {
+		return executions == 1
+	}, 1*time.Second, 100*time.Millisecond)
+
+	fakeClock.Advance(24 * time.Hour)
+
+	r.Eventually(func() bool {
+		return executions == 2
+	}, 1*time.Second, 100*time.Millisecond)
+
+	scheduler.Shutdown()
+}
